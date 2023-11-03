e10d5c92af7de2ce485170d58d37427b5c154897
alexandru.grama <galex486@gmail.com>
2023-11-03 13:51:49 +0200

    Up

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,33 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+//          Exercițiu 1
             java.util.List<String> programari = new ArrayList<>();
-            programari.add("10:00 la medic");
-            programari.add("11:00 la analize medicale");
-            programari.add("13:00 la cursuri de IT");
-            programari.add("15:00 intilnire cu un coleg");
-            programari.add("17:00 la Bazinul de inot Dinamo");
+            programari.add("10:00 la medic |");
+            programari.add("11:00 la analize medicale |");
+            programari.add("13:00 la cursuri de IT |");
+            programari.add("15:00 intilnire cu un coleg |");
+            programari.add("17:00 la Bazinul de inot Dinamo |");
 
-            System.out.println(programari);
+            System.out.println("Exercițiu 1: " + programari);
+
+//      Exercițiu 3
+        Map<String, String> Dictionar = new HashMap<>();
+        Dictionar.put("The country", " țară");
+        Dictionar.put("Sky", " cer");
+        Dictionar.put("Dog", " cîine");
+        Dictionar.put("Blade", " lamă");
+        Dictionar.put("Tiger", " tigru");
+        Dictionar.put("Sea", " mare");
+        Dictionar.put("River", " rîu");
+        Dictionar.put("Lake", " lac");
+        Dictionar.put("Soup", " supă");
+        Dictionar.put("Ice cream", " înghețată");
+
+        System.out.println("Exercițiu 3: " + Dictionar);
+
 
 
 
