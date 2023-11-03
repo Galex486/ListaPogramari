8d74de14e9fd48abd70a509a9a9cbaa6e27d4993
alexandru.grama <galex486@gmail.com>
2023-11-03 11:47:13 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+            java.util.List<String> programari = new ArrayList<>();
+            programari.add("10:00 la medic");
+            programari.add("11:00 la analize medicale");
+            programari.add("13:00 la cursuri de IT");
+            programari.add("15:00 intilnire cu un coleg");
+            programari.add("17:00 la Bazinul de inot Dinamo");
+
+            System.out.println(programari);
+
+
+
+
+
+    }
+
+}
